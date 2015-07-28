1e241d3f6f58659e7a2d07fc02a2823e4d570152
thetruthmyg <thetruthmyg94@gmail.com>
2015-07-28 11:29:39 +0800

    完善登陆注册退出登录功能，目前可保持登陆状态。

--- app/src/main/java/com/ehelp/user/pinyin/SettingActivity.java
+++ app/src/main/java/com/ehelp/user/pinyin/SettingActivity.java
@@ -1,7 +1,9 @@
 package com.ehelp.user.pinyin;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
@@ -10,6 +12,7 @@ import android.widget.Toast;
 
 import com.ehelp.R;
 import com.ehelp.account.login;
+import com.ehelp.home.Home;
 
 public class SettingActivity extends ActionBarActivity implements OnChangedListener{
     //TOOLbar
@@ -44,7 +47,13 @@ public class SettingActivity extends ActionBarActivity implements OnChangedListe
         startActivity(intent);
     }
     public void exit_click (View v) {
+        SharedPreferences sharedPreferences = this.getSharedPreferences("user_id", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt("user_id", -1);
+        editor.commit();
+
         Intent intent = new Intent(this, login.class);
         startActivity(intent);
+        this.finish();
     }
 }
