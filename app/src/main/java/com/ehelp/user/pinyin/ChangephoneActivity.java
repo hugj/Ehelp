8732c0cd426f596fb62a7e3d555edcdbc3f3b0c9
raaaywind <419274318@qq.com>
2015-07-29 11:21:27 +0800

    2个图标

--- app/src/main/java/com/ehelp/user/pinyin/ChangephoneActivity.java
+++ app/src/main/java/com/ehelp/user/pinyin/ChangephoneActivity.java
@@ -13,6 +13,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.ehelp.R;
