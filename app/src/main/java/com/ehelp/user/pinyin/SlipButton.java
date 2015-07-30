b9a729edd7d3eaf92d32c521072ed70238a0bba8
wang04_09 <wang04_09@163.com>
2015-07-30 23:50:24 +0800

    重要修改！恢复正常推送，推送按键修复。

--- app/src/main/java/com/ehelp/user/pinyin/SlipButton.java
+++ app/src/main/java/com/ehelp/user/pinyin/SlipButton.java
@@ -51,15 +51,15 @@ public class SlipButton extends View implements OnTouchListener {
 
     private void init(){//初始化
         SharedPref = getContext().getSharedPreferences("slip_button", Context.MODE_PRIVATE);
-        NowChoose = SharedPref.getBoolean("slip_button", false);
+        NowChoose = SharedPref.getBoolean("slip_button", true);
         //载入图片资源
-        bg_off = BitmapFactory.decodeResource(getResources(), R.drawable.split_on);
-        bg_on = BitmapFactory.decodeResource(getResources(), R.drawable.split_off);
+        bg_on = BitmapFactory.decodeResource(getResources(), R.drawable.split_on);
+        bg_off = BitmapFactory.decodeResource(getResources(), R.drawable.split_off);
         slip_btn = BitmapFactory.decodeResource(getResources(), R.drawable.split_button);
         //获得需要的Rect数据,绘制的矩形
-        Btn_Off = new Rect(0,0,slip_btn.getWidth(),slip_btn.getHeight());
-        Btn_On = new Rect(bg_on.getWidth()-slip_btn.getWidth(), 0,
-                bg_on.getWidth(), slip_btn.getHeight());
+        Btn_On = new Rect(0,0,slip_btn.getWidth(),slip_btn.getHeight());
+        Btn_Off = new Rect(bg_off.getWidth()-slip_btn.getWidth(), 0,
+                bg_off.getWidth(), slip_btn.getHeight());
         setOnTouchListener(this);//设置监听器
     }
 
