9e7b03064dbfdd5b441c0d6cc714baf59bd41677
hugj <huuugj@163.com>
2015-07-31 11:43:03 +0800

    我参与的FAB

---
+++ app/src/main/java/com/ehelp/user/history/HistoryAdapter.java
@@ -0,0 +1,190 @@
+package com.ehelp.user.history;
+
+import android.content.Context;
+import android.os.StrictMode;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.ehelp.R;
+import com.ehelp.entity.answer;
+import com.ehelp.utils.RequestHandler;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by gj on 2015/7/31.
+ */
+public class HistoryAdapter extends BaseAdapter {
+
+    private Context context=null;
+    private List<Map<String,Object>> list = new ArrayList<Map<String,Object>>();
+    private Map<String,Object> item;
+    private String tit;
+    private double longitude;
+    private double latitude;
+    private int event_id;
+    private int type;
+    private List<answer> answerList;
+    Gson gson = new Gson();
+
+    HistoryAdapter(Context context, int id){
+        this.context=context;
+        event_id = id;
+
+        // 使用后台线程运行网络连接功能
+        StrictMode.setThreadPolicy(
+                new StrictMode.ThreadPolicy.Builder().
+                        detectDiskReads().detectDiskWrites().detectNetwork().penaltyLog().build());
+        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().
+                detectLeakedSqlLiteObjects().detectLeakedClosableObjects().
+                penaltyLog().penaltyDeath().build());
+
+        //数据初始化
+        setList();
+    }
+
+
+    public void setList() {
+        //数据初始化
+        try {
+            String jsonStrng = "{" +
+                    "\"event_id\":" + event_id + "}";
+            String message = RequestHandler.sendPostRequest(
+                    "http://120.24.208.130:1501/event/anslist", jsonStrng);
+            if (message == "false") {
+                item=new HashMap<String,Object>();
+                item.put("头像", R.drawable.icon);
+                item.put("内容", "连接失败，请检查网络是否连接并重试");
+                item.put("用户", "");
+                item.put("时间", "");
+                item.put("采纳", "");
+                list.add(item);
+            } else {
+                JSONObject j1 = new JSONObject(message);
+                if (j1.getInt("status") == 500) {
+                    item=new HashMap<String,Object>();
+                    item.put("头像", R.drawable.icon);
+                    item.put("内容", "因为迷之原因拉取不到回答。。。");
+                    item.put("用户", "");
+                    item.put("时间", "");
+                    item.put("采纳", "");
+                    list.add(item);
+                }
+                String st = j1.getString("answer_list");
+                answerList = gson.fromJson(st, new TypeToken<List<answer>>(){}.getType());
+                for (int i = 0; i < answerList.size(); i++) {
+                    //获取昵称
+                    final int user_id = answerList.get(i).getAuthoridId();
+                    String nickname = "";
+                    String jsonStrng1 = "{" +
+                            "\"id\":" + user_id + "}";
+                    final String message1 = RequestHandler.sendPostRequest(
+                            "http://120.24.208.130:1501/user/get_information", jsonStrng1);
+                    if (message1 == "false") {
+                        nickname = "联网失败找不到昵称了。。。";
+                    }   else {
+                        try {
+                            JSONObject jO = new JSONObject(message1);
+                            nickname = jO.getString("nickname");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    //添加条目
+                    item=new HashMap<String,Object>();
+                    item.put("头像", R.drawable.icon);
+                    item.put("内容", answerList.get(i).getContent());
+                    item.put("用户", nickname);
+                    item.put("时间", answerList.get(i).getTime());
+                    if (answerList.get(i).getIs_adopted() == 1) {
+                        item.put("采纳", "被采纳答案");
+                    }
+                    item.put("采纳", "");
+                    list.add(item);
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getCount() {return list.size();}
+    public Object getItem(int position) {return position;}
+    public long getItemId(int position) {return position;}
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        //头像
+        ImageView iv=new ImageView(context);
+        RelativeLayout.LayoutParams lp_iv=new RelativeLayout.LayoutParams(80,80);
+        lp_iv.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+        iv.setLayoutParams(lp_iv);
+        iv.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+        iv.setImageResource((Integer) ((list.get(position)).get("头像")));
+        //内容
+        TextView title=new TextView(context);
+        RelativeLayout.LayoutParams lp_tv=new RelativeLayout.LayoutParams(-2,-2);
+        lp_tv.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+        title.setLayoutParams(lp_tv);
+        title.setTextSize(title.getTextSize());
+        title.setText((String) (list.get(position).get("内容")));
+        //用户
+        TextView user=new TextView(context);
+        RelativeLayout.LayoutParams lp_sex=new RelativeLayout.LayoutParams(-2,-2);
+        lp_sex.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+        user.setLayoutParams(lp_sex);
+        user.setText((list.get(position).get("用户")) + "");
+        //时间
+        TextView time=new TextView(context);
+        RelativeLayout.LayoutParams lp_time=new RelativeLayout.LayoutParams(-2,-2);
+        lp_sex.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+        time.setLayoutParams(lp_sex);
+        time.setText((String)(list.get(position).get("时间")));
+        //采纳
+        TextView  Reward=new TextView(context);
+        RelativeLayout.LayoutParams lp_age=new RelativeLayout.LayoutParams(-2,-2);
+        lp_age.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+        Reward.setLayoutParams(lp_age);
+        Reward.setText((list.get(position).get("采纳")) + "");
+
+        //线性布局1
+        LinearLayout l11=new LinearLayout(context);
+        l11.setLayoutParams(new LinearLayout.LayoutParams(300, 100));
+        l11.setOrientation(LinearLayout.VERTICAL);
+        l11.addView(user);
+        l11.addView(time);
+        //线性布局2
+        LinearLayout l12=new LinearLayout(context);
+        l12.setLayoutParams(new LinearLayout.LayoutParams(400, -2));
+        l12.setOrientation(LinearLayout.HORIZONTAL);
+        l12.addView(iv);
+        l12.addView(l11);
+        //相对布局1
+        RelativeLayout rl1=new RelativeLayout(context);
+        rl1.setLayoutParams(new RelativeLayout.LayoutParams(700,-2));
+        rl1.addView(l12);
+        rl1.addView(Reward);
+        //返回视图
+        LinearLayout returnView=new LinearLayout(context);
+        returnView.setLayoutParams(new ListView.LayoutParams(-1,-2));//注意:ListView.LayoutParams
+        returnView.setOrientation(LinearLayout.VERTICAL);
+        returnView.addView(rl1);
+        returnView.addView(title);
+
+        return returnView;
+    }
+
+}
--- app/src/main/java/com/ehelp/user/history/MyHistory.java
+++ app/src/main/java/com/ehelp/user/history/MyHistory.java
@@ -1,33 +1,50 @@
 package com.ehelp.user.history;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.os.Bundle;
 import android.os.Parcelable;
+import android.os.StrictMode;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ehelp.R;
+import com.ehelp.map.sendhelp_map;
+import com.ehelp.send.CountNum;
+import com.ehelp.send.SendQuestion;
 import com.ehelp.utils.RequestHandler;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import android.content.SharedPreferences;
-import android.widget.Toast;
+import com.wangjie.androidbucket.utils.ABTextUtil;
+import com.wangjie.androidbucket.utils.imageprocess.ABShape;
+import com.wangjie.androidinject.annotation.annotations.base.AILayout;
+import com.wangjie.androidinject.annotation.annotations.base.AIView;
+import com.wangjie.androidinject.annotation.present.AIActionBarActivity;
+import com.wangjie.rapidfloatingactionbutton.RapidFloatingActionButton;
+import com.wangjie.rapidfloatingactionbutton.RapidFloatingActionHelper;
+import com.wangjie.rapidfloatingactionbutton.RapidFloatingActionLayout;
+import com.wangjie.rapidfloatingactionbutton.contentimpl.labellist.RFACLabelItem;
+import com.wangjie.rapidfloatingactionbutton.contentimpl.labellist.RapidFloatingActionContentLabelList;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class MyHistory extends ActionBarActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+@AILayout(R.layout.activity_my_history)
+public class MyHistory extends AIActionBarActivity implements RapidFloatingActionContentLabelList.OnRapidFloatingActionContentLabelListListener {
+    @AIView(R.id.label_list_sample_rfal)
+    private RapidFloatingActionLayout rfaLayout;
+    @AIView(R.id.label_list_sample_rfab)
+    private RapidFloatingActionButton rfaButton;
+    private RapidFloatingActionHelper rfabHelper;
 
     private ViewPager mPager;//页卡内容
     private List<View> listViews; // Tab页面列表
@@ -41,11 +58,22 @@ public class MyHistory extends ActionBarActivity {
     private Toolbar mToolbar;
 
     private int wha = 2;//1代表全部，2代表发起，3代表接收
+    public final static String EXTRA_MESSAGE = "com.ehelp.receive.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_history);
+        init();
+    }
+
+    private void init() {
+        StrictMode.setThreadPolicy(
+                new StrictMode.ThreadPolicy.Builder().
+                        detectDiskReads().detectDiskWrites().detectNetwork().penaltyLog().build());
+        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().
+                detectLeakedSqlLiteObjects().detectLeakedClosableObjects().
+                penaltyLog().penaltyDeath().build());
+
         //set toolbar
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         mToolbar.setTitle("");
@@ -56,6 +84,85 @@ public class MyHistory extends ActionBarActivity {
         SharedPref = this.getSharedPreferences("user_id", MODE_PRIVATE);
         InitViewPager();
         InitTextView();
+
+        //set FAB
+        fab();
+    }
+
+    private void fab(){
+        RapidFloatingActionContentLabelList rfaContent = new RapidFloatingActionContentLabelList(context);
+        rfaContent.setOnRapidFloatingActionContentLabelListListener(this);
+        List<RFACLabelItem> items = new ArrayList<>();
+        items.add(new RFACLabelItem<Integer>()
+                        .setLabel("求救")
+                        .setResId(R.mipmap.ic_launcher)
+                        .setIconNormalColor(0xffd84315)
+                        .setIconPressedColor(0xffbf360c)
+                        .setWrapper(0)
+        );
+        items.add(new RFACLabelItem<Integer>()
+                        .setLabel("求助")
+//                        .setResId(R.mipmap.ico_test_c)
+                        .setDrawable(getResources().getDrawable(R.mipmap.ic_launcher))
+                        .setIconNormalColor(0xff4e342e)
+                        .setIconPressedColor(0xff3e2723)
+                        .setLabelColor(Color.WHITE)
+                        .setLabelSizeSp(14)
+                        .setLabelBackgroundDrawable(ABShape.generateCornerShapeDrawable(0xaa000000, ABTextUtil.dip2px(context, 4)))
+                        .setWrapper(1)
+        );
+        items.add(new RFACLabelItem<Integer>()
+                        .setLabel("提问")
+                        .setResId(R.mipmap.ic_launcher)
+                        .setIconNormalColor(0xff056f00)
+                        .setIconPressedColor(0xff0d5302)
+                        .setLabelColor(0xff056f00)
+                        .setWrapper(2)
+        );
+        rfaContent
+                .setItems(items)
+                .setIconShadowRadius(ABTextUtil.dip2px(context, 5))
+                .setIconShadowColor(0xff888888)
+                .setIconShadowDy(ABTextUtil.dip2px(context, 5))
+        ;
+
+        rfabHelper = new RapidFloatingActionHelper(
+                context,
+                rfaLayout,
+                rfaButton,
+                rfaContent
+        ).build();
+    }
+    @Override
+    public void onRFACItemLabelClick(int position, RFACLabelItem item) {
+        if (position == 0) {
+            Intent intent = new Intent(this, CountNum.class);
+            startActivity(intent);
+        } else
+        if (position == 1) {
+            Intent intent = new Intent(this, sendhelp_map.class);
+            startActivity(intent);
+        } else {
+            Intent intent = new Intent(this, SendQuestion.class);
+            startActivity(intent);
+        }
+        rfabHelper.toggleContent();
+    }
+
+    @Override
+    public void onRFACItemIconClick(int position, RFACLabelItem item) {
+        if (position == 0) {
+            Intent intent = new Intent(this, CountNum.class);
+            startActivity(intent);
+        } else
+        if (position == 1) {
+            Intent intent = new Intent(this, sendhelp_map.class);
+            startActivity(intent);
+        } else {
+            Intent intent = new Intent(this, SendQuestion.class);
+            startActivity(intent);
+        }
+        rfabHelper.toggleContent();
     }
 
     @Override
