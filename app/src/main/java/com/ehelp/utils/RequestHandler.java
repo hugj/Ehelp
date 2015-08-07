9d17cc8cf4158432333f677be7d340cf7ca80533
hugj <huuugj@163.com>
2015-08-07 10:30:36 +0800

    更新

--- app/src/main/java/com/ehelp/utils/RequestHandler.java
+++ app/src/main/java/com/ehelp/utils/RequestHandler.java
@@ -1,4 +1,3 @@
-package com.ehelp.utils;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
@@ -29,17 +28,17 @@ public class RequestHandler {
 	 * 		   a string "false"/null indicate some errors happened
 	 */
 	public static String sendGetRequest(String urlString){
-		
+
 		try {
-			
+
 			URL url = new URL(urlString);
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			conn.setConnectTimeout(1000);
+			conn.setConnectTimeout(500);
 			conn.setRequestMethod("GET");
 			conn.setDoInput(true); // permit to use the inputstream
 			conn.setDoOutput(true); // permit to use the outputstrem
 			conn.setUseCaches(false); // deny to use the cache
-			
+
 			StringBuffer sBuffer = new StringBuffer();
 			if(conn.getResponseCode() == HttpURLConnection.HTTP_OK){
 				String line = null;
@@ -50,16 +49,16 @@ public class RequestHandler {
 				}
 				return sBuffer.toString();
 			}
-			
+
 		} catch (Exception e) {
-		
+
 			e.printStackTrace();
-			
+
 		}
-		
+
 		return "false";
 	}
-	
+
 	/* send the request by POST method
 	 * @param a url path
 	 * 		  for the ehelp project, must provide the url path
@@ -69,21 +68,21 @@ public class RequestHandler {
 	 * 		   a string "false"/null indicate some errors happened
 	 */
 	public static String sendPostRequest(String urlString, String jsondata){
-		
+
 		try {
-			
+
 			URL url = new URL(urlString);
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			conn.setConnectTimeout(1000);
+			conn.setConnectTimeout(500);
 			conn.setRequestMethod("POST");
 			conn.setDoInput(true); // permit to use the inputstream
 			conn.setDoOutput(true); // permit to use the outputstrem
 			conn.setUseCaches(false); // deny to use the cache
 			conn.setRequestProperty("Content-Type", "application/json");// set the request Content-Type
-			
+
 			byte data[] = jsondata.getBytes("UTF-8"); // use utf-8 coding format to transformat string to a byte array
 			conn.getOutputStream().write(data);
-			
+
 			StringBuffer sBuffer = new StringBuffer();
 			if(conn.getResponseCode() == HttpURLConnection.HTTP_OK){
 				String line = null;
@@ -94,16 +93,15 @@ public class RequestHandler {
 				}
 				return sBuffer.toString();
 			}
-			
+
 		} catch (Exception e) {
-			
+
 			e.printStackTrace();
-			return e.toString();
 
 		}
-		
+
 		return "false";
-		
+
 	}
 	
 	/* send a file(.jpg) to server by POST method
@@ -112,16 +110,16 @@ public class RequestHandler {
 	 * @return a String "true" if upload successfully,0
 	 * 		   "false" otherwise
 	 */
-	
+
 	public static String uploadFile(String urlString, File file) {
-		
+
 		String PREFIX = "--";
 		String LINE_END = "\r\n";
 		String BOUNDARY = UUID.randomUUID().toString(); // set the boundary
-		
-		
+
+
 		try {
-			
+
 			URL url = new URL(urlString);
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 			conn.setConnectTimeout(50000);
@@ -130,27 +128,27 @@ public class RequestHandler {
 			conn.setDoInput(true); // permit to use the inputstream
 			conn.setDoOutput(true); // permit to use the outputstrem
 			conn.setUseCaches(false); // deny to use the cache
-			conn.setRequestProperty("Charset", "utf-8"); 
-			conn.setRequestProperty("Content-Type", "multipart/form-data" + ";boundary="
+			conn.setRequestProperty("Charset", "utf-8");
+			conn.setRequestProperty("Content-Type", "multipart/form-data"+";boundary="
 					+ BOUNDARY);// set the request Content-Type
 			conn.setRequestProperty("connection", "keep-alive");
-			
+
 			if(file != null) {
 				OutputStream outputStream = conn.getOutputStream();
 				DataOutputStream doStream = new DataOutputStream(outputStream);
-				
+
 				// request's entity body
 				StringBuffer sBuffer = new StringBuffer();
 				sBuffer.append(PREFIX);
 				sBuffer.append(BOUNDARY);
-				sBuffer.append(LINE_END);		
-				sBuffer.append("Content-Disposition: form-data; name=\"img\"; filename=\""
+				sBuffer.append(LINE_END);
+				sBuffer.append("Content-Disposition: form-data; name=\"avatar\"; filename=\""
 						+ file.getName() + "\"" + LINE_END);
 				sBuffer.append("Content-Type: application/octet-stream; charset = utf-8" + LINE_END);
 				sBuffer.append(LINE_END);
-				
+
 				doStream.write(sBuffer.toString().getBytes());
-				
+
 				InputStream in = new FileInputStream(file);
 				byte[] bytes = new byte[2014];
 				int len = 0;
@@ -163,25 +161,32 @@ public class RequestHandler {
 				doStream.write(
 						(PREFIX + BOUNDARY + PREFIX + LINE_END).getBytes());
 				doStream.flush();
-				
-				if(conn.getResponseCode() == 200) {
-					return "true";
+
+				StringBuffer resBuffer = new StringBuffer();
+				if(conn.getResponseCode() == HttpURLConnection.HTTP_OK){
+					String line = null;
+					InputStream resin = conn.getInputStream();
+					BufferedReader bReader = new BufferedReader(new InputStreamReader(resin));
+					while((line = bReader.readLine()) != null) {
+						resBuffer.append(line);
+					}
+					return resBuffer.toString();
 				}
-				
+
 			}
-			
-			
+
+
 		} catch (MalformedURLException e) {
-			
+
 			e.printStackTrace();
-			
+
 		} catch (IOException e) {
 
 			e.printStackTrace();
-			
+
 		}
-		
+
 		return "false";
-		
+
 	}
 }
