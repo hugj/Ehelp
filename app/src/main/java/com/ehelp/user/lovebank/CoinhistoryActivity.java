1d36854274e83a8a0b73099c127bbd1f5725bc26
Sarahhuang <srahhuang67@gmail.com>
2015-08-13 13:31:52 +0800

    爱心币历史交易记录

---
+++ app/src/main/java/com/ehelp/user/lovebank/CoinhistoryActivity.java
@@ -0,0 +1,293 @@
+package com.ehelp.user.lovebank;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.os.StrictMode;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import com.ehelp.R;
+import com.ehelp.entity.Event;
+import com.ehelp.map.sendhelp_map;
+import com.ehelp.send.CountNum;
+import com.ehelp.send.SendQuestion;
+import com.wangjie.androidbucket.utils.ABTextUtil;
+import com.wangjie.androidbucket.utils.imageprocess.ABShape;
+import com.wangjie.androidinject.annotation.annotations.base.AILayout;
+import com.wangjie.androidinject.annotation.annotations.base.AIView;
+import com.wangjie.androidinject.annotation.present.AIActionBarActivity;
+import com.wangjie.rapidfloatingactionbutton.RapidFloatingActionButton;
+import com.wangjie.rapidfloatingactionbutton.RapidFloatingActionHelper;
+import com.wangjie.rapidfloatingactionbutton.RapidFloatingActionLayout;
+import com.wangjie.rapidfloatingactionbutton.contentimpl.labellist.RFACLabelItem;
+import com.wangjie.rapidfloatingactionbutton.contentimpl.labellist.RapidFloatingActionContentLabelList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AILayout(R.layout.activity_coinhistory)
+public class CoinhistoryActivity extends AIActionBarActivity implements RapidFloatingActionContentLabelList.OnRapidFloatingActionContentLabelListListener {
+    @AIView(R.id.label_list_sample_rfal)
+    private RapidFloatingActionLayout rfaLayout;
+    @AIView(R.id.label_list_sample_rfab)
+    private RapidFloatingActionButton rfaButton;
+    private RapidFloatingActionHelper rfabHelper;
+
+    private SharedPreferences sharedPref;
+    private int user_id;
+    private List<Event> events;
+
+    private ViewPager mPager;//页卡内容
+    private List<View> listViews; // Tab页面列表
+    private TextView t1, t2, t3;// 页卡头标
+    //后台连接
+    private SharedPreferences SharedPref;
+    private String message;
+    private String jsonStrng;
+
+    //toolbar
+    private Toolbar mToolbar;
+
+    //private int wha = 1;//1代表发起，2代表接收
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        init();
+    }
+
+    private void init() {
+        StrictMode.setThreadPolicy(
+                new StrictMode.ThreadPolicy.Builder().
+                        detectDiskReads().detectDiskWrites().detectNetwork().penaltyLog().build());
+        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().
+                detectLeakedSqlLiteObjects().detectLeakedClosableObjects().
+                penaltyLog().penaltyDeath().build());
+
+        //set toolbar
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setTitle("");
+        setSupportActionBar(mToolbar);
+        TextView tvv =(TextView) findViewById(R.id.titlefortoolbar);
+        tvv.setText("爱心币交易记录");
+
+        sharedPref = this.getSharedPreferences("user_id", Context.MODE_PRIVATE);
+        user_id = sharedPref.getInt("user_id", -1);
+        InitViewPager();
+        InitTextView();
+
+        //set FAB
+        fab();
+    }
+
+    private void fab(){
+        RapidFloatingActionContentLabelList rfaContent = new RapidFloatingActionContentLabelList(context);
+        rfaContent.setOnRapidFloatingActionContentLabelListListener(this);
+        List<RFACLabelItem> items = new ArrayList<>();
+        items.add(new RFACLabelItem<Integer>()
+                        .setLabel("求救")
+                        .setResId(R.mipmap.ic_launcher)
+                        .setIconNormalColor(0xffd84315)
+                        .setIconPressedColor(0xffbf360c)
+                        .setWrapper(0)
+        );
+        items.add(new RFACLabelItem<Integer>()
+                        .setLabel("求助")
+//                        .setResId(R.mipmap.ico_test_c)
+                        .setDrawable(getResources().getDrawable(R.mipmap.ic_launcher))
+                        .setIconNormalColor(0xff4e342e)
+                        .setIconPressedColor(0xff3e2723)
+                        .setLabelColor(Color.WHITE)
+                        .setLabelSizeSp(14)
+                        .setLabelBackgroundDrawable(ABShape.generateCornerShapeDrawable(0xaa000000, ABTextUtil.dip2px(context, 4)))
+                        .setWrapper(1)
+        );
+        items.add(new RFACLabelItem<Integer>()
+                        .setLabel("提问")
+                        .setResId(R.mipmap.ic_launcher)
+                        .setIconNormalColor(0xff056f00)
+                        .setIconPressedColor(0xff0d5302)
+                        .setLabelColor(0xff056f00)
+                        .setWrapper(2)
+        );
+        rfaContent
+                .setItems(items)
+                .setIconShadowRadius(ABTextUtil.dip2px(context, 5))
+                .setIconShadowColor(0xff888888)
+                .setIconShadowDy(ABTextUtil.dip2px(context, 5))
+        ;
+
+        rfabHelper = new RapidFloatingActionHelper(
+                context,
+                rfaLayout,
+                rfaButton,
+                rfaContent
+        ).build();
+    }
+    @Override
+    public void onRFACItemLabelClick(int position, RFACLabelItem item) {
+        if (position == 0) {
+            Intent intent = new Intent(this, CountNum.class);
+            startActivity(intent);
+        } else
+        if (position == 1) {
+            Intent intent = new Intent(this, sendhelp_map.class);
+            startActivity(intent);
+        } else {
+            Intent intent = new Intent(this, SendQuestion.class);
+            startActivity(intent);
+        }
+        rfabHelper.toggleContent();
+    }
+
+    @Override
+    public void onRFACItemIconClick(int position, RFACLabelItem item) {
+        if (position == 0) {
+            Intent intent = new Intent(this, CountNum.class);
+            startActivity(intent);
+        } else
+        if (position == 1) {
+            Intent intent = new Intent(this, sendhelp_map.class);
+            startActivity(intent);
+        } else {
+            Intent intent = new Intent(this, SendQuestion.class);
+            startActivity(intent);
+        }
+        rfabHelper.toggleContent();
+    }
+
+    /**
+     * 初始化头标
+     */
+
+    private void InitTextView() {
+        t1 = (TextView) findViewById(R.id.coin_text1); //转账头标
+        t2 = (TextView) findViewById(R.id.coin_text2); //交易头标
+
+        t1.setOnClickListener(new MyOnClickListener(0));
+        t2.setOnClickListener(new MyOnClickListener(1));
+    }
+    /**
+     * 头标点击监听
+     */
+    public class MyOnClickListener implements View.OnClickListener {
+        private int index = 0;
+        public MyOnClickListener(int i) {
+            index = i; //判断是哪个
+        }
+        @Override
+        public void onClick(View v) {
+            mPager.setCurrentItem(index);
+        }
+    }
+    /**
+     * 初始化ViewPager
+     */
+    private void InitViewPager() {
+        mPager = (ViewPager) findViewById(R.id.vPager1);
+        listViews = new ArrayList<View>();
+        LayoutInflater mInflater = getLayoutInflater();
+        listViews.add(mInflater.inflate(R.layout.coin_historylist1, null));
+        listViews.add(mInflater.inflate(R.layout.coin_historylist2, null));
+        mPager.setAdapter(new MyPagerAdapter(listViews));
+        mPager.setCurrentItem(0);
+        mPager.setOnPageChangeListener(new MyOnPageChangeListener());
+        //初始化，使默认页面卡是是绿色的
+        TextView tv = (TextView) findViewById(R.id.coin_text1);
+        tv.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+    }
+    /**
+     * ViewPager适配器
+     */
+    public class MyPagerAdapter extends PagerAdapter {
+        public List<View> mListViews;
+
+        public MyPagerAdapter(List<View> mListViews) {
+            this.mListViews = mListViews;
+        }
+
+        @Override
+        public void destroyItem(View arg0, int arg1, Object arg2) {
+            ((ViewPager) arg0).removeView(mListViews.get(arg1));
+        }
+
+        @Override
+        public void finishUpdate(View arg0) {
+        }
+
+        @Override
+        public int getCount() {
+            return mListViews.size();
+        }
+
+        @Override
+        public Object instantiateItem(View arg0, int arg1) {
+            ((ViewPager) arg0).addView(mListViews.get(arg1), 0);
+            return mListViews.get(arg1);
+        }
+
+        @Override
+        public boolean isViewFromObject(View arg0, Object arg1) {
+            return arg0 == (arg1);
+        }
+
+        @Override
+        public void restoreState(Parcelable arg0, ClassLoader arg1) {
+        }
+
+        @Override
+        public Parcelable saveState() {
+            return null;
+        }
+
+        @Override
+        public void startUpdate(View arg0) {
+        }
+    }
+    /**
+     * 页卡切换监听
+     */
+    public class MyOnPageChangeListener implements ViewPager.OnPageChangeListener {
+
+        @Override
+        public void onPageSelected(int arg0) {
+            TextView tv = (TextView) findViewById(R.id.text1);
+            TextView tv2 = (TextView) findViewById(R.id.text2);
+            TextView tv3 = (TextView) findViewById(R.id.text3);
+            switch (arg0) {
+                case 0:
+                    tv.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+                    tv2.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                    tv3.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                    //添加该页面可能的事项。比如跳转之类
+                    break;
+                case 1:
+                    tv.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                    tv2.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+                    tv3.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                    break;
+                case 2:
+                    tv.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                    tv2.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                    tv3.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+                    break;
+            }
+        }
+
+        @Override
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int arg0) {
+        }
+    }
+}
